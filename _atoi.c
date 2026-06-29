ebaf989ee41bbb264ba6c10b44ecb0311e197ead
stephenp0320 <stephen power>
2026-06-29 13:58:24 +0100

    feat: basic working atoi implemented

--- _atoi.c
+++ _atoi.c
@@ -6,7 +6,7 @@ int my_atoi(char *string){
 	for (i = 0; string[i] != '\0'; i++){
 		res = (res * 10) + (string[i] - '0');
 	}
-	return 0;
+	return res;
 }
 
 int main(int argc, char *argv[]){
@@ -17,7 +17,8 @@ int main(int argc, char *argv[]){
 
 	char *input_str = argv[1];
 	int conversion = my_atoi(input_str);
-	printf("string version: %s", input_str);
+	printf("string version: %s\n", input_str);
 	printf("integer version: %d", conversion);
+	printf("\n");
 	return 0;
 }
