9daba41937a54665442bfd217cbb78faaa1b59ab
stephenp0320 <stephen power>
2026-06-29 14:09:29 +0100

    feat: add handling for negatives

--- _atoi.c
+++ _atoi.c
@@ -2,9 +2,20 @@
 
 int my_atoi(char *string){
 	int res = 0;
-	int i;
-	for (i = 0; string[i] != '\0'; i++){
+	int i = 0;
+	int is_negative = 0;
+	if (string[0] == '-'){
+		is_negative = 1;
+		i = 1;
+	}
+	for (; string[i] != '\0'; i++){
+		if (string[i] < '0' || string[i] > '9'){
+			break;
+		}
 		res = (res * 10) + (string[i] - '0');
+	}
+	if (is_negative){
+		res = -res;
 	}
 	return res;
 }
